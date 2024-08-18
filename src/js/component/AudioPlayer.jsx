daad37d979e578109271a20aa1cd9484093e9244
zz-ederfs <161877224+zz-ederfs@users.noreply.github.com>
2024-08-18 07:41:50 +0000

    working next song , but not as smooth yet

--- src/js/component/AudioPlayer.jsx
+++ src/js/component/AudioPlayer.jsx
@@ -1,43 +1,26 @@
 import React, { useEffect, useRef, useState } from "react";
 
-export function AudioPlayer() {
-  const [songList, setSongList] = useState([]);
-  const [currentSong, setCurrentSong] = useState("");
+export function AudioPlayer(props) {
+  /* let getSong = useRef(); */
 
-  let playSong = new Audio(currentSong);
-  let getSong = useRef();
-  useEffect(() => {
-    getSongs();
-  }, []);
-
-  const getURL = (e) => {
-    //console.log(songList[0]);
-    setCurrentSong(songList[e].url);
-  };
-
-  function getSongs() {
-    const URL = "https://playground.4geeks.com/sound/songs";
-
-    fetch(URL, { method: "GET" })
-      .then((response) => response.json())
-      .then((data) => setSongList(data.songs));
-  }
-  //console.log(songList);
-
-  const createPlaylist = songList.map((value, index) => {
+  const createPlaylist = props.songlist.map((value, index) => {
     return (
       <div className="row row-cols-1 px-0 mx-0 px-sm-0 mx-sm-0 px-md-0 px-lg-0 mx-lg-0  border-bottom  border-light border-1 border-opacity-25">
         <div
           className="col d-flex  p-3 align-items-center"
           style={{ backgroundColor: "#0d1b2a" }}
-          ref={getSong}
-          onClick={(e) => getURL(e.target.id)}
+          /* ref={getSong} */
+          onClick={(e) => {
+            props.setIndex(e.target.id);
+            props.geturl(e.target.id);
+          }}
           id={index}
         >
           <p
             className="text-light ms-2 mb-0 mb-md-0 mb-lg-0 "
             onClick={(e) => {
-              getURL(e.target.parentElement.id);
+              props.setIndex(e.target.parentElement.id);
+              props.geturl(e.target.parentElement.id);
               e.stopPropagation();
             }}
           >
@@ -46,7 +29,8 @@ export function AudioPlayer() {
           <p
             className="text-light ms-5 mb-0 mb-md-0 mb-lg-0"
             onClick={(e) => {
-              getURL(e.target.parentElement.id);
+              props.setIndex(e.target.parentElement.id);
+              props.geturl(e.target.parentElement.id);
               e.stopPropagation();
             }}
           >
@@ -60,8 +44,34 @@ export function AudioPlayer() {
   return createPlaylist;
 }
 
-export function MediaControls() {
-  const buttonPlay = useRef();
+export function MediaControls(props) {
+  const buttonPlay = useRef(null);
+
+  function reproduceSong() {
+    props.playsong.play();
+  }
+
+  function pauseSong() {
+    props.playsong.pause();
+  }
+
+  function nextSong() {
+    let playListSize = props.songlist.length;
+    if (props.index < playListSize - 1) {
+      let nextIndex = parseInt(props.index) + 1;
+      console.log("debug valor ", nextIndex);
+      props.geturl(nextIndex);
+      reproduceSong();
+    } else {
+      props.geturl(0);
+      reproduceSong();
+    }
+
+    //console.log(playListSize);
+  }
+  /*   useEffect(() => {
+    props.playsong.pause();
+  }, [buttonPlay.current.onClick()]); */
 
   return (
     <div className="container sticky-bottom">
@@ -75,15 +85,22 @@ export function MediaControls() {
               <i
                 className="fa-solid fa-backward-step fa-xl me-5"
                 style={{ color: "#ffffff" }}
+                onClick={() => pauseSong()}
               ></i>
               <i
-                className="fa-regular fa-circle-play fa-xl"
+                className="fa-regular fa-circle-play fa-xl  "
                 ref={buttonPlay}
-                style={{ color: "#ffffff" }} /* onClick={} */
+                style={{ color: "#ffffff" }}
+                onClick={() => reproduceSong()}
+              ></i>
+              <i
+                class="fa-regular fa-circle-pause fa-xl d-none"
+                style={{ color: "#ffffff" }}
               ></i>
               <i
                 className="fa-solid fa-forward-step fa-xl ms-5"
                 style={{ color: "#ffffff" }}
+                onClick={() => nextSong()}
               ></i>
             </span>
           </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useRef } from "react";
 import { AudioPlayer } from "./AudioPlayer";
 import { NavBar } from "./NavBar";
 import { MediaControls } from "./AudioPlayer";
+import { useState } from "react";
+import { useEffect } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+  const [songList, setSongList] = useState([]);
+  const [currentSong, setCurrentSong] = useState("");
+  const [currentIndex, setCurrentIndex] = useState("");
+
+  const playSong = new Audio(currentSong);
+
+  useEffect(() => {
+    getSongs();
+  }, []);
+
+  const getURL = (e) => {
+    //console.log(songList[0]);
+    setCurrentSong("https://playground.4geeks.com" + songList[e].url);
+    console.log(currentSong);
+  };
+
+  function getSongs() {
+    const URL = "https://playground.4geeks.com/sound/songs";
+
+    fetch(URL, { method: "GET" })
+      .then((response) => response.json())
+      .then((data) => setSongList(data.songs));
+  }
   return (
     <div
       className=" vh-100 vh-md-100 vh-lg-100 py-2"
@@ -18,10 +43,19 @@ const Home = () => {
       >
         <div className="row row-cols-md-1">
           <NavBar />
-          <AudioPlayer />
+          <AudioPlayer
+            songlist={songList}
+            geturl={getURL}
+            setIndex={setCurrentIndex}
+          />
         </div>
       </div>
-      <MediaControls />
+      <MediaControls
+        songlist={songList}
+        playsong={playSong}
+        index={currentIndex}
+        geturl={getURL}
+      />
     </div>
   );
 };
